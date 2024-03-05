87de0c9c93895ea3537d63f954d30ba86805e5ed
meris <meris.bahti@gmail.com>
2024-03-05 22:19:56 +0100

    handle errors a little better

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,15 @@ const executablePath = process.env["EXECUTABLE_PATH"];
 if (!username || !password || !executablePath)
   throw new Error("Both `USERNAME` and `PASSWORD` need to be defined in en`");
 
-const jwtToken = await getSkanetrafikenToken({
-  username,
-  password,
-  executablePath,
-});
+const useFakeToken = true;
+
+const jwtToken = useFakeToken
+  ? "faketesttoken"
+  : await getSkanetrafikenToken({
+      username,
+      password,
+      executablePath,
+    });
 
 const result = await fetch(
   "https://www.skanetrafiken.se/gw-bns/registered-app",
@@ -21,7 +25,13 @@ const result = await fetch(
     headers: { Authorization: `Bearer ${jwtToken}` },
   },
 )
-  .then((x) => x.json())
+  .then((x) =>
+    !x.ok
+      ? Promise.reject(
+          JSON.stringify({ status: x.status, statusText: x.statusText }),
+        )
+      : x.json(),
+  )
   .then((x) => ResponseSchema.parse(x));
 
 console.log(JSON.stringify(result, null, 2));
