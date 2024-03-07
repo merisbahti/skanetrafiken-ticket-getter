7ac9866a872e5149cb95224a5481f91e5a8082e7
meris <meris.bahti@gmail.com>
2024-03-07 21:52:48 +0100

    expressify

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import "dotenv";
 import { getSkanetrafikenToken } from "./getSkanetrafikenToken";
 import { TicketsResponseSchema } from "./types";
 import { getActiveTickets } from "./getActiveTickets";
+import express from "express";
 
+const app = express();
+
+const port = process.env.PORT || 3000;
 const username = process.env["USERNAME"];
 const password = process.env["PASSWORD"];
 const executablePath = process.env["EXECUTABLE_PATH"];
@@ -10,31 +14,39 @@ const executablePath = process.env["EXECUTABLE_PATH"];
 if (!username || !password || !executablePath)
   throw new Error("Both `USERNAME` and `PASSWORD` need to be defined in en`");
 
-const jwtToken = await getSkanetrafikenToken({
-  username,
-  password,
-  executablePath,
+app.get("/", async (req, res) => {
+  const jwtToken = await getSkanetrafikenToken({
+    username,
+    password,
+    executablePath,
+  });
+
+  const result = await fetch(
+    "https://www.skanetrafiken.se/gw-bns/registered-app",
+    {
+      headers: { Authorization: `Bearer ${jwtToken}` },
+    },
+  )
+    .then((x) =>
+      !x.ok
+        ? Promise.reject(
+            JSON.stringify({ status: x.status, statusText: x.statusText }),
+          )
+        : x.json(),
+    )
+    .then((x) => TicketsResponseSchema.parse(x));
+
+  res.send(
+    JSON.stringify({
+      activeTickets: getActiveTickets(result.tickets, new Date()).map(
+        (x) => x.activeTo,
+      ),
+    }),
+  );
 });
 
-const result = await fetch(
-  "https://www.skanetrafiken.se/gw-bns/registered-app",
-  {
-    headers: { Authorization: `Bearer ${jwtToken}` },
-  },
-)
-  .then((x) =>
-    !x.ok
-      ? Promise.reject(
-          JSON.stringify({ status: x.status, statusText: x.statusText }),
-        )
-      : x.json(),
-  )
-  .then((x) => TicketsResponseSchema.parse(x));
-
-console.log(JSON.stringify(result, null, 2));
-console.log(
-  "active ticket dates:",
-  getActiveTickets(result.tickets, new Date()).map((x) => x.activeTo),
-);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 // await page.screenshot({ path: "example.png" });
